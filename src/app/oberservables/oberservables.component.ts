9a62d6aa331384caaed9e30ffbe9fa4aa38674e1
Alexander Schwartz <git@aSchwartz.de>
2021-01-06 11:12:19 +0100

    Adding a custom observable. End of chapter 173.

--- src/app/oberservables/oberservables.component.ts
+++ src/app/oberservables/oberservables.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-oberservables',
@@ -9,7 +9,10 @@ import { interval, Subscription } from 'rxjs';
 export class OberservablesComponent implements OnInit, OnDestroy {
 
   private counterSubscription: Subscription | undefined
+  private customCounterSubscription: Subscription | undefined
   count: number = 0
+  customCount: number = 0
+  customError: string = ''
   constructor() { }
 
   ngOnInit(): void {
@@ -17,9 +20,41 @@ export class OberservablesComponent implements OnInit, OnDestroy {
       this.count = count
       console.log(count)
     })
+
+    const customObervable = new Observable(observer => {
+      // This is the code that sends the event
+      let count = 0
+      setInterval(() => {
+        observer.next(count)
+
+        // Alternative functions
+        // observer.error()
+        // observer.complete()
+        if (count > 5)
+          observer.error(new Error('Count is bigger than 5. Stopping. I\'m outta here.'))
+        count++
+      }, 900)
+      
+      // Note: The observable will never reach 7, because it stops at 6, when it throws an error.
+      if (count == 7) {
+        observer.complete()
+      }
+
+    })
+
+    this.customCounterSubscription = customObervable.subscribe(data => {
+      // this is, what happens, when a new event is received
+      this.customCount = <number><any>data;
+    }, error => {
+      this.customError = (<Error>error).message
+    }, () => {
+      // Completed
+      console.log('Completed');      
+    })
   }
 
   ngOnDestroy(): void {
     this.counterSubscription?.unsubscribe()
+    this.customCounterSubscription?.unsubscribe()
   }
 }
