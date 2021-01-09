15a1fc6bfc5cd037898e51097ebae00bd8034684
Alexander Schwartz <git@aSchwartz.de>
2021-01-09 01:52:39 +0100

    Enabling auto login. Chapter 303

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Ingredient} from './model/ingredient.model';
 import {LoggingService} from './services/logging.service';
 import {ShoppingService} from './services/shopping.service';
+import {AuthService} from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   lastAddedIngredient: Ingredient | undefined;
   title = 'Angular Recipe Book';
 
-  constructor(private loggingService: LoggingService, private shoppingService: ShoppingService) {
+  constructor(private loggingService: LoggingService, private shoppingService: ShoppingService, private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.autoLogin();
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -15,42 +15,76 @@ export interface AuthResponseData {
   registered?: boolean;
 }
 
+const LOCAL_STORAGE_KEY = 'userData';
+const URL_SIGN_UP = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp';
+const URL_SIGN_IN = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword';
+const API_KEY = 'AIzaSyALO01jLzk5PTc-m0Wynz3nEvZRzXJ07tI';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  urlSignUp = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp';
-  urlSignIn = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword';
-  apiKey = 'AIzaSyALO01jLzk5PTc-m0Wynz3nEvZRzXJ07tI';
   userSubject = new BehaviorSubject<User | null>(null);
 
   constructor(private http: HttpClient, private router: Router) {
   }
 
   signUp(email: string, password: string): Observable<AuthResponseData> {
-    return this.http.post<AuthResponseData>(this.urlSignUp + '?key=' + this.apiKey, {email, password, returnSecureToken: 'true'})
+    return this.http.post<AuthResponseData>(URL_SIGN_UP + '?key=' + API_KEY, {email, password, returnSecureToken: 'true'})
       .pipe(catchError(this.handleError), tap(responseData => {
-          this.handleAuthentication(responseData, password);
+          this.handleAuthentication(responseData);
+          // The previous line does the same as the following three lines:
+          // .pipe(catchError(errorResponse => {
+          //   return this.handleError(errorResponse);
+          // }));
         }
       ));
-    // The previous line does the same as the following three lines:
-    // .pipe(catchError(errorResponse => {
-    //   return this.handleError(errorResponse);
-    // }));
+  }
+
+  autoLogin(): void {
+    const userDataRaw = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (!userDataRaw) {
+      return;
+    }
+    const userData: {
+      id: string,
+      name: string,
+      email: string,
+      password: string,
+      gender: string,
+      roles: string[],
+      coronaAttitude: string,
+      Token: string,
+      tokenExpirationDate?: Date
+    } = JSON.parse(userDataRaw);
+    const loadedUser = new User(
+      userData.id,
+      userData.name,
+      userData.email,
+      userData.password,
+      userData.gender,
+      userData.roles,
+      userData.coronaAttitude,
+      userData.Token,
+      userData.tokenExpirationDate);
+    if (loadedUser.token) {
+      this.userSubject.next(loadedUser);
+    }
   }
 
   signIn(email: string, password: string): Observable<AuthResponseData> {
-    return this.http.post<AuthResponseData>(this.urlSignIn + '?key=' + this.apiKey, {email, password, returnSecureToken: 'true'})
+    return this.http.post<AuthResponseData>(URL_SIGN_IN + '?key=' + API_KEY, {email, password, returnSecureToken: 'true'})
       .pipe(catchError(this.handleError), tap(responseData => {
-        this.handleAuthentication(responseData, password);
+        this.handleAuthentication(responseData);
       }));
   }
 
-  private handleAuthentication(responseData: AuthResponseData, password: string): any {
+  private handleAuthentication(responseData: AuthResponseData): any {
     const expirationDate = new Date(new Date().getTime() + responseData.expiresIn * 1000);
-    const user = new User(responseData.localId, '', responseData.email, password, '', [], '', responseData.idToken, expirationDate);
+    const user = new User(responseData.localId, '', responseData.email, '', '', [], '', responseData.idToken, expirationDate);
     this.userSubject.next(user);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(user));
   }
 
   private handleError(errorResponse: HttpErrorResponse): Observable<never> {
@@ -66,5 +100,6 @@ export class AuthService {
   logout(): void {
     this.userSubject.next(null);
     this.router.navigate(['/auth']);
+    localStorage.setItem(LOCAL_STORAGE_KEY, '');
   }
 }
