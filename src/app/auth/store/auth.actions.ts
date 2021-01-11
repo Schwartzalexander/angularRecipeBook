dac273709f78c2e5a7e6b92ace164b81d32ecc56
Alexander Schwartz <git@aSchwartz.de>
2021-01-11 03:27:46 +0100

    Prefixing action names. End of chapter 363.

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,4 +1,4 @@
 import {createAction, props} from '@ngrx/store';
 
-export const login = createAction('Logging in', props<{ email: string, userId: string, token: string, expirationTimestamp: number }>());
-export const logout = createAction('Logging out');
+export const login = createAction('[Auth] Logging in', props<{ email: string, userId: string, token: string, expirationTimestamp: number }>());
+export const logout = createAction('[Auth] Logging out');
--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,9 +1,9 @@
 import {createAction, props} from '@ngrx/store';
 import {Ingredient} from '../../shared/model/ingredient.model';
 
-export const addIngredient = createAction('Adding ingredient', props<{ ingredient: Ingredient }>());
-export const addIngredients = createAction('Adding ingredients', props<{ ingredients: Ingredient[] }>());
-export const updateIngredient = createAction('Updating ingredient', props<{ ingredient: Ingredient }>());
-export const deleteIngredient = createAction('Deleting ingredient');
-export const startEdit = createAction('Starting editing ingredient', props<{ id: number }>());
-export const stopEdit = createAction('Stopping editing ingredient');
+export const addIngredient = createAction('[ShoppingList] Adding ingredient', props<{ ingredient: Ingredient }>());
+export const addIngredients = createAction('[ShoppingList] Adding ingredients', props<{ ingredients: Ingredient[] }>());
+export const updateIngredient = createAction('[ShoppingList] Updating ingredient', props<{ ingredient: Ingredient }>());
+export const deleteIngredient = createAction('[ShoppingList] Deleting ingredient');
+export const startEdit = createAction('[ShoppingList] Starting editing ingredient', props<{ id: number }>());
+export const stopEdit = createAction('[ShoppingList] Stopping editing ingredient');
