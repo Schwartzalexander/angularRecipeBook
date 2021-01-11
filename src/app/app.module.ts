dac490ff0084040334d3701f3b7fb4e8040638c2
Alexander Schwartz <git@aSchwartz.de>
2021-01-11 22:00:04 +0100

    Adding store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import {StoreModule} from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import {EffectsModule} from '@ngrx/effects';
 import {AuthEffects} from './auth/store/auth.effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -26,6 +28,7 @@ import {AuthEffects} from './auth/store/auth.effects';
     AuthModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument(({logOnly: environment.production})),
     SharedModule,
     AppRoutingModule,
     ErrorsModule,
