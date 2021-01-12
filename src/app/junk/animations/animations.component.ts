51dd682b86345d2758830b946fe1152c46b48425
Alexander Schwartz <git@aSchwartz.de>
2021-01-12 22:12:31 +0100

    Better animations. End of chapter 413

--- src/app/junk/animations/animations.component.ts
+++ src/app/junk/animations/animations.component.ts
@@ -1,71 +1,141 @@
 import {Component, OnInit} from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {animate, AnimationEvent, group, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-animations',
   templateUrl: './animations.component.html',
   styleUrls: ['./animations.component.css'],
   animations: [
-    trigger('buttonState', [
+    trigger('divState', [
       state('normal', style({
         'background-color': 'red',
         transform: 'translateX(0)'
       })),
-      state('highlight', style({
+      state('highlighted', style({
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
-      transition('normal => highlight', animate(300)),
-      transition('highlight => normal', animate(800))
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
     ]),
     trigger('wildState', [
       state('normal', style({
         'background-color': 'red',
-        transform: 'translateX(0)'
+        transform: 'translateX(0) scale(1)'
       })),
-      state('highlight', style({
+      state('highlighted', style({
         'background-color': 'blue',
-        transform: 'translateX(100px)'
+        transform: 'translateX(100px) scale(1)'
       })),
       state('shrunken', style({
         'background-color': 'green',
         transform: 'translateX(0) scale(0.5)'
       })),
-      transition('normal => highlight', animate(300)),
-      transition('highlight => normal', animate(800)),
-      // transition('normal <=> *', animate(2000, style({
-      //   borderRadius: '50px'
-      // })))
-      transition('normal <=> *', [
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
           style({
-            'background-color': 'orange',
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
           }),
-          animate(1000, style({
-            borderRadius: '50px'
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        group([
+          animate(300, style({
+            color: 'red'
           })),
-          animate(500)
-        ]
-      )
-    ])
+          animate(800, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          }))
+        ])
+      ])
+    ]),
   ]
 })
 export class AnimationsComponent implements OnInit {
-
   state = 'normal';
   wildState = 'normal';
-
-  constructor() {
-  }
+  list = ['Milk', 'Sugar', 'Bread'];
 
   ngOnInit(): void {
   }
 
   onAnimate(): void {
-    this.state = this.state === 'normal' ? 'highlight' : 'normal';
-    this.wildState = this.wildState === 'normal' ? 'highlight' : 'normal';
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
   }
 
   onShrink(): void {
-    this.wildState = this.wildState === 'shrunken' ? 'normal' : 'shrunken';
+    this.wildState = 'shrunken';
   }
+
+  onAdd(item: string): void {
+    this.list.push(item);
+  }
+
+  onDelete(item: string): void {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+
+  animationStarted(event: AnimationEvent): void {
+    console.log(event);
+  }
+
+  animationEnded(event: AnimationEvent): void {
+    console.log(event);
+  }
+
 }
