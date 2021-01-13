3dbf0d72661c4cb028eb7405e8beffe2bb6f150f
Alexander Schwartz <git@aSchwartz.de>
2021-01-13 13:16:22 +0100

    WIP: Rehydration

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,16 +1,19 @@
 import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
 import * as fromAuth from '../auth/store/auth.reducer';
 import * as fromRecipes from '../recipes/store/recipes.reducer';
+import * as fromHydration from './hydration/hydration.reducer';
 import {ActionReducerMap} from '@ngrx/store';
 
 export interface AppState {
   shoppingList: fromShoppingList.State;
   auth: fromAuth.State;
-  recipes: fromRecipes.State
+  recipes: fromRecipes.State;
+  hydration: fromHydration.State;
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
   shoppingList: fromShoppingList.shoppingListReducer,
   auth: fromAuth.authReducer,
-  recipes: fromRecipes.recipesReducer
+  recipes: fromRecipes.recipesReducer,
+  hydration: fromHydration.hydrationMetaReducer
 };
---
+++ src/app/store/hydration/hydration.effect.ts
@@ -0,0 +1,48 @@
+import {Injectable} from '@angular/core';
+import {Actions, createEffect, ofType, OnInitEffects} from '@ngrx/effects';
+import {Action, Store} from '@ngrx/store';
+import {distinctUntilChanged, map, switchMap, tap} from 'rxjs/operators';
+import {AppState} from '../app.reducer';
+import {hydrate, hydrateFailure, hydrateSuccess} from './hydration.action';
+
+@Injectable()
+export class HydrationEffects implements OnInitEffects {
+  hydrate$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(hydrate),
+      map(() => {
+        const storageValue = localStorage.getItem('state');
+        if (storageValue) {
+          try {
+            const state = JSON.parse(storageValue);
+            return hydrateSuccess({state});
+          } catch {
+            localStorage.removeItem('state');
+          }
+        }
+        return hydrateFailure();
+      })
+    )
+  );
+
+  serialize$ = createEffect(
+    () =>
+      this.action$.pipe(
+        ofType(
+          hydrateSuccess,
+          hydrateFailure
+        ),
+        switchMap(() => this.store),
+        distinctUntilChanged(),
+        tap(state => localStorage.setItem('state', JSON.stringify(state)))
+      ),
+    {dispatch: false}
+  );
+
+  constructor(private action$: Actions, private store: Store<AppState>) {
+  }
+
+  ngrxOnInitEffects(): Action {
+    return hydrate();
+  }
+}
